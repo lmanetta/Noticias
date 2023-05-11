973986605cc39ba9ecdaf3130363a02e60175d8a
lmanetta <luchymanetta92@hotmail.com>
2023-05-11 19:55:08 -0300

    Modificaciones

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-// use App\Factories\NoticiaFactory;
+use App\Factories\NoticiaFactory;
 use App\Models\Noticia;
+use App\Models\User;
 
 class NoticiaController extends Controller
 {
@@ -24,5 +25,41 @@ class NoticiaController extends Controller
             'imagen' => "https://servicepath.co/wp-content/uploads/2019/11/news-1200x565.jpg",
             'id' => $id);
         return view('backend.noticia.show', compact('noticia'));
+    }
+
+    public function create()
+    {
+        $users = User::pluck('name', 'id');
+        return view('backend.noticias.create', compact('users'));
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate(
+            [
+                'titulo' => 'required|unique:noticias',
+                'cuerpo' => 'required',
+                'autor' => 'required',
+                'image' => 'image|max:2048'
+            ]
+        );
+
+        $noticia = new Noticia();
+        $noticia->titulo = $request->input('titulo');
+        $noticia->cuerpo = $request->input('cuerpo');
+        $archivoImagen = $request->file('imagen');
+        $noticia->autor = $request->input('autor');
+        $noticia->save();
+        //php artisan storage:link
+        if ($request->hasFile('imagen')) {
+            $archivoImagen = $request->file('imagen');
+            $path = $archivoImagen->storeAs('public/noticias/' . $noticia->id, $archivoImagen->getClientOriginalName());
+            $savedPath = str_replace("public/", "", $path);
+            $noticia->imagen = $savedPath;
+            $noticia->save();
+        }
+
+        $request->session()->flash('status', 'Se guardó correctamente la noticia ' . $noticia->titulo);
+        return redirect()->route('noticias.create');
     }
 }
---
+++ resources/views/backend/noticia/create.blade.php
@@ -0,0 +1,47 @@
+@extends('backend.layouts.main')
+
+@section('title', 'Nueva Noticia')
+
+@section('content')
+    <h1>Nueva Noticia</h1>
+    <div>
+        @if (Session::has('status'))
+            <div class="alert alert-success">{{ Session('status') }}</div>
+        @endif
+    </div>
+    <div class="links">
+        {{ Form::open(['route' => 'noticias.store', 'files' => true]) }}
+        @csrf
+        <div class="form-group @if ($errors->has('titulo')) has-error has-feedback @endif">
+            {{ Form::label('titulo', 'Título', ['class' => 'control-label']) }}
+            {{ Form::text('titulo', old('titulo'), ['class' => 'form-control', 'placeholder' => 'Ingrese el Título']) }}
+            @error('titulo')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            {{ Form::label('cuerpo', 'Cuerpo', ['class' => 'control-label']) }}
+            {{ Form::textarea('cuerpo', old('cuerpo'), ['class' => 'form-control', 'placeholder' => 'Ingrese el Cuerpo']) }}
+            @error('cuerpo')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group @if ($errors->has('imagen')) has-error has-feedback @endif">
+            {{ Form::label('imagen', 'Imagen', ['class' => 'control-label']) }}
+            {{ Form::file('imagen') }}
+            @error('image')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            {{ Form::label('autor', 'Autor', ['class' => 'control-label']) }}
+            {{ Form::select('autor', $users, null, ['class' => 'form-control', 'placeholder' => 'Seleccione un autor']) }}
+            @error('autor')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+    </div>
+    </br><button type="submit" style="width: 100%;" class="btn btn-primary">{{ __('noticias.guardar') }}</button></div>
+    {!! Form::close() !!}
+@endsection
